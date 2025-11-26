4608657d80378b2dbb932e6db6369ce8503bc095
Gabriele <gmercolino2003@gmail.com>
2025-11-26 10:33:12 +0100

    chore: removed bluetooth command as no longer necessary

--- commands.ts
+++ commands.ts
@@ -3,11 +3,9 @@ const REBOOT_COMMAND = "systemctl reboot"
 const LOCK_COMMAND = "swaylock"
 
 const AUDIO_COMMAND = "pavucontrol"
-const BLUETOOTH_COMMAND = "blueman-manager"
 
 export const shutdownCommand = () => SHUTDOWN_COMMAND
 export const rebootCommand = () => REBOOT_COMMAND
 export const lockCommand = () => LOCK_COMMAND
 
 export const audioCommand = () => AUDIO_COMMAND
-export const bluetoothCommand = () => BLUETOOTH_COMMAND
