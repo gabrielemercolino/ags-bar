31a49fd900c508092b918615c38b830f07e051d2
Gabriele <gmercolino2003@gmail.com>
2025-11-29 10:11:52 +0100

    feat: added tooltip to battery widget

--- widget/Status.tsx
+++ widget/Status.tsx
@@ -36,13 +36,24 @@ const battery = AstalBattery.get_default()
 export function Battery() {
   const charging = createBinding(battery, "charging")
   const percentage = createBinding(battery, "percentage")
+  const timeToFull = createBinding(battery, "timeToFull").as((t) =>
+    t > 0 ? `Full in ${secondsToHM(t)}` : "Full",
+  )
+  const timeToEmpty = createBinding(battery, "timeToEmpty").as(
+    (t) => `Empty in ${secondsToHM(t)}`,
+  )
 
-  const both = createComputed([charging, percentage], (c, p) => {
-    return {
-      charging: c,
-      percentage: p,
-    }
-  })
+  const merged = createComputed(
+    [charging, percentage, timeToFull, timeToEmpty],
+    (c, p, ttf, tte) => {
+      return {
+        charging: c,
+        percentage: p,
+        timeToFull: ttf,
+        timeToEmpty: tte,
+      }
+    },
+  )
 
   function getIcon(percentage: number, charging: boolean) {
     const chargingIcons = ["󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂊", "󰂋", "󰂅"]
@@ -52,14 +63,27 @@ export function Battery() {
     return charging ? chargingIcons[index] : defaultIcons[index]
   }
 
+  function secondsToHM(seconds: number) {
+    var hours = Math.floor((seconds % (3600 * 24)) / 3600)
+    var minutes = Math.floor((seconds % 3600) / 60)
+
+    console.log(hours, minutes)
+
+    const h = hours <= 0 ? "" : `${hours}h, `
+    const m = minutes <= 0 ? "" : `${minutes}m`
+
+    return h + m
+  }
+
   return (
-    <With value={both}>
-      {({ charging, percentage }) => {
+    <With value={merged}>
+      {({ charging, percentage, timeToFull, timeToEmpty }) => {
         const rounded = Math.round(percentage * 100)
         return (
           <box
             visible={battery.isPresent}
             cssName="battery"
+            tooltipText={charging ? timeToFull : timeToEmpty}
             class={charging ? "charging" : ""}
           >
             {`${getIcon(rounded, charging)} ${rounded}%`}
