ad4adb31837870fae44ca49a28a643b9195cbe8e
Gabriele <gmercolino2003@gmail.com>
2025-12-09 23:09:05 +0100

    removal: removed audio command

--- commands.ts
+++ commands.ts
@@ -2,10 +2,6 @@ const SHUTDOWN_COMMAND = "systemctl poweroff"
 const REBOOT_COMMAND = "systemctl reboot"
 const LOCK_COMMAND = "swaylock"
 
-const AUDIO_COMMAND = "pavucontrol"
-
 export const shutdownCommand = () => SHUTDOWN_COMMAND
 export const rebootCommand = () => REBOOT_COMMAND
 export const lockCommand = () => LOCK_COMMAND
-
-export const audioCommand = () => AUDIO_COMMAND
