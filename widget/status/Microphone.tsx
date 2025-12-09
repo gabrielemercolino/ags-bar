652938bb7fe60506a44de05f5f4fab219a293e6f
Gabriele <gmercolino2003@gmail.com>
2025-12-09 23:08:27 +0100

    feat: added microphone widget

---
+++ widget/status/Microphone.tsx
@@ -0,0 +1,92 @@
+import AstalWp from "gi://AstalWp";
+import { createBinding, createComputed, For, With } from "ags";
+import { Gtk } from "ags/gtk4";
+import Pango from "gi://Pango";
+
+const wireplumber = AstalWp.get_default()
+
+export default function Microphone() {
+  const all_microphones = createBinding(wireplumber, "nodes")
+    .as(nodes => nodes.filter(n => n.get_media_class() === AstalWp.MediaClass.AUDIO_SOURCE))
+
+
+  return (
+    <menubutton cssName="audio-input">
+      <DefaultMicrophoneWidget />
+      <popover cssName="pop-up">
+        <box
+          cssName="microphones"
+          orientation={Gtk.Orientation.VERTICAL}
+        >
+          <For each={all_microphones}>
+            {microphone => <MicrophoneEntry microphone={microphone} />}
+          </For>
+        </box>
+      </popover>
+    </menubutton>
+  )
+}
+
+
+function DefaultMicrophoneWidget() {
+  const default_microphone = createBinding(wireplumber, "default_microphone")
+
+  return (
+    <With value={default_microphone}>
+      {
+        (microphone) => {
+          const muted = createBinding(microphone, "mute")
+          const volume = createBinding(microphone, "volume")
+          const microphone_state = createComputed(() => ({ muted: muted(), volume: volume() }))
+
+          return (
+            <label
+              label={microphone_state.as(({ muted, volume }) =>
+                `${getIcon(muted)} ${Math.round(volume * 100)}%`)}
+            />
+          )
+        }
+      }
+    </With>
+  )
+}
+
+
+type MicrophoneEntryProps = {
+  microphone: AstalWp.Node
+}
+function MicrophoneEntry({ microphone }: MicrophoneEntryProps) {
+  const muted = createBinding(microphone, "mute")
+  const volume = createBinding(microphone, "volume")
+
+  return (
+    <box css_name="microphone" spacing={8}>
+      <button onClicked={() => microphone.set_mute(!microphone.get_mute())}>
+        <label
+          cssName="icon"
+          label={muted.as((muted) => getIcon(muted))}
+          tooltipMarkup={muted.as(m => m ? "unmute" : "mute")}
+        />
+      </button>
+
+      <label
+        cssName="name"
+        label={microphone.get_description() ?? "no name"}
+        maxWidthChars={10}
+        ellipsize={Pango.EllipsizeMode.END}
+        tooltipText={microphone.get_description() ?? "no name"}
+      />
+
+      <slider
+        value={volume}
+        hexpand
+        onChangeValue={(_source, _scroll_type, val) => microphone.set_volume(val)}
+      />
+    </box>
+  )
+}
+
+function getIcon(muted: boolean) {
+  if (muted) return "󰍭"
+  return ""
+}
