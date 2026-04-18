23f088a5cd7d8c424afe51423788243a7229d555
gabriele <gmercolino2003@gmail.com>
2026-04-18 10:38:13 +0200

    chore: formatted flake

--- flake.nix
+++ flake.nix
@@ -10,57 +10,63 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ags,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-    astalPackages = with ags.packages.${system}; [
-      io
-      astal4
-      hyprland
-      network
-      bluetooth
-      wireplumber
-      battery
-      tray
-      notifd
-    ];
+      astalPackages = with ags.packages.${system}; [
+        io
+        astal4
+        hyprland
+        network
+        bluetooth
+        wireplumber
+        battery
+        tray
+        notifd
+      ];
 
-    extraPackages = astalPackages ++ [pkgs.gjs];
+      extraPackages = astalPackages ++ [ pkgs.gjs ];
 
-    ags' = ags.packages.${system}.default.override {
-      inherit extraPackages;
-    };
-  in {
-    packages.${system} = {
-      default = pkgs.callPackage ./package.nix {
+      ags' = ags.packages.${system}.default.override {
         inherit extraPackages;
-        gitRev = self.rev or "dirty";
-        ags = ags';
+      };
+    in
+    {
+      packages.${system} = {
+        default = pkgs.callPackage ./package.nix {
+          inherit extraPackages;
+          gitRev = self.rev or "dirty";
+          ags = ags';
+        };
+      };
+
+      homeManagerModules.default = import ./home-module.nix {
+        basePackage = self.packages.${system}.default;
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            ags'
+            pkgs.gjs
+          ];
+
+          shellHook = ''
+            echo
+            echo "To generate node_modules 'ags types -d . -u'"
+            echo "To generate types run 'ags types -d .'"
+            echo "To run the bar 'ags run app.ts'"
+            echo
+          '';
+        };
       };
     };
-
-    homeManagerModules.default = import ./home-module.nix {
-      basePackage = self.packages.${system}.default;
-    };
-
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        buildInputs = [ags' pkgs.gjs];
-
-        shellHook = ''
-          echo
-          echo "To generate node_modules 'ags types -d . -u'"
-          echo "To generate types run 'ags types -d .'"
-          echo "To run the bar 'ags run app.ts'"
-          echo
-        '';
-      };
-    };
-  };
 }
