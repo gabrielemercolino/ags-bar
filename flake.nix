48ddbfb7a54ca474044d7942f1c55c4cfba6168a
gabriele <ciruzzo032@noreply.codeberg.org>
2026-07-23 10:19:38 +0200

    chore: updated lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Ags bar";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/421eebfd0ec7bccd4abe826ce62d7e6e83129493";
     ags = {
       url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
